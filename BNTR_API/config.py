a63e2d512e0548b1a322ee4fe8808aaad15f0035
Colin McGravey <mcgravey@umich.edu>
2023-10-31 17:18:25 -0400

    whatever

--- BNTR_API/config.py
+++ BNTR_API/config.py
@@ -3,6 +3,8 @@ import pathlib
 
 API_KEY = '87ab0a3db51d297d3d1cf2d4dcdcb71b'
 
+APPLICATION_ROOT = '/api/routes/'
+
 BNTR_API_ROOT = pathlib.Path(__file__).resolve().parent.parent
 UPLOAD_FOLDER = BNTR_API_ROOT/'var'/'uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
